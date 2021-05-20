854089aef6391279d1ed927c66fe86404d9ed98c
김성일 <gimseong-il@ilMm.local>
2021-05-20 16:59:04 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+// font
+import 'package:google_fonts/google_fonts.dart';
+// screen
 import 'package:neighbor_library/app.dart';
-import 'package:neighbor_library/screens/home_screen.dart';
-
+// binding
 import 'binding/instance_binding.dart';
+// firebase
+import 'package:firebase_core/firebase_core.dart';
+// controller
+import 'services/controller/auth_controller.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
     initialBinding: InstanceBinding(),
+    theme: ThemeData(
+      textTheme: GoogleFonts.notoSansTextTheme(),
+    ),
     home: MyApp(),
   ));
 }
 
 class MyApp extends StatelessWidget {
+  final authController = Get.put(AuthController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: App(),
-    );
+    return FutureBuilder(
+        future: authController.checkUserLoggedIn(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error Processing');
+          }
+          if (snapshot.hasData) {
+            return snapshot.data;
+          }
+          return buildLoading();
+        });
   }
+
+  Widget buildLoading() => Center(child: CircularProgressIndicator());
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -31,14 +32,113 @@ class HomeScreen extends StatelessWidget {
             ),
             body: TabBarView(
               children: [
-                Container(
-                    child: Column(
+                ListView(
                   children: [
-                    Text('aa'),
-                    Text('aa'),
-                    Text('aa'),
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Color(0xFFEDDBDB),
+                            ),
+                            height: Get.height / 2,
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        width: 30,
+                                        height: 30,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(100),
+                                          color: Color(0xFF0AFF00),
+                                        ),
+                                      ),
+                                      Text(
+                                        '현재 1권 이용중',
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            color: Color(0xFF4A4A4A)),
+                                      ),
+                                    ],
+                                  ),
+                                  Column(
+                                    children: [
+                                      Container(
+                                        margin: EdgeInsets.only(top: 30),
+                                        width: Get.width / 2,
+                                        height: Get.height / 2 - 170,
+                                        color: Colors.black,
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            top: 14, bottom: 10),
+                                        child: Text(
+                                          '작은 별이지만 빛나고 있어',
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                      Text('초록기린님의 책')
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 60),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '이도를 사용하는 이유',
+                                  style: TextStyle(
+                                    fontSize: Get.width / 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 22),
+                                  height: 96,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Color(0xFFFFDFB8),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        '고마워요\n이웃님',
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                            height: 1.5),
+                                      ),
+                                      Text('bb'),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
-                )),
+                ),
                 Text('bb'),
               ],
             ),
